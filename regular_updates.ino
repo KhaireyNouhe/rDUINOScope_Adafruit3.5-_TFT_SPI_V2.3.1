42ff2897b9ad8bc42712d2c2b6bd92c74384eadf
Khairey Nouhe <31562987+KhaireyNouhe@users.noreply.github.com>
2019-03-06 21:54:31 +0300

    Update regular_updates.ino

--- regular_updates.ino
+++ regular_updates.ino
@@ -22,98 +22,123 @@
 //  - On each TimeInterval (~10 sec.) - Updates screens accordingly (e.g. Time, LST, Temperature, Humidity and etc.)
 //
 
-void considerTempUpdates() // Temperature && Humidity Updates
-{                                
-  if (CURRENT_SCREEN == 4 && (millis()-Tupdate_time) > 30000)
+void considerTempUpdates() // Temperature && Humidity Updates && battery voltage
+{
+  if (CURRENT_SCREEN == 4 && (millis() - Tupdate_time) > 30000)
   {
-       float tHum = dht.readHumidity();
-       float tTemp = dht.readTemperature();
-       if (isnan(tHum) || isnan(tTemp))
-       {
-          return;
-       }
-       else
-       {
-          //_temp = tTemp - 2;  // I need to calibrate my sensor... it reads 2 deg. higher temp.
-          _temp = tTemp;
-          _humid = tHum;
-       }
-       tft.setTextSize(2);
-       tft.setTextColor(title_texts);
-       if (_temp > -75 && _temp < 75 && _humid < 100 && _humid > 0)
-       {
-         tft.fillRect(258,35,30,55, title_bg);
-           
-         tft.setCursor(261, 38);
-         tft.print(_temp,0);
-        
-         tft.setCursor(261, 70);
-         tft.print(_humid,0);
-       }
-       Tupdate_time = millis();
+    float tHum = dht.readHumidity();
+    float tTemp = dht.readTemperature();
+    if (isnan(tHum) || isnan(tTemp))
+    {
+      return;
+    }
+    else
+    {
+      //_temp = tTemp - 2;  // I need to calibrate my sensor... it reads 2 deg. higher temp.
+      _temp = tTemp;
+      _humid = tHum;
+    }
+
+    tft.setTextSize(2);
+    tft.setTextColor(title_texts);
+    if (_temp > -75 && _temp < 75 && _humid < 100 && _humid > 0)
+    {
+      tft.fillRect(258, 35, 30, 55, title_bg);
+
+      tft.setCursor(261, 38);
+      tft.print(_temp, 0);
+
+      tft.setCursor(261, 70);
+      tft.print(_humid, 0);
+    }
+    Tupdate_time = millis();
+
+#ifdef use_battery_level
+    drawBatteryLevel(260, 120, calculateBatteryLevel());
+#endif
   }
 }
 
-void considerTimeUpdates(){   // UPDATEs time on Screen1 && Screen4 -  Clock Screen and Main Screen
-   int changes=0;
-   for (int y=0; y<12; y++){
-       if (w_DateTime[y]!=0){
-           changes=1;
-       }
-   }
-  int mer_flp_sound = HAHour + ((HAMin + MIN_TO_MERIDIAN_FLIP + MIN_SOUND_BEFORE_FLIP)/60);
-  if((millis()- update_time) > 10000 && mer_flp_sound >= 24){
-    if (IS_SOUND_ON){
-      SoundOn(3830,8);
+void considerTimeUpdates()
+{ // UPDATEs time on Screen1 && Screen4 -  Clock Screen and Main Screen
+  int changes = 0;
+  for (int y = 0; y < 12; y++)
+  {
+    if (w_DateTime[y] != 0)
+    {
+      changes = 1;
     }
   }
-  if (CURRENT_SCREEN == 4 && (millis()-update_time) > 10000){
-      tft.setTextSize(1);
+  int mer_flp_sound = HAHour + ((HAMin + MIN_TO_MERIDIAN_FLIP + MIN_SOUND_BEFORE_FLIP) / 60);
+  if ((millis() - update_time) > 10000 && mer_flp_sound >= 24)
+  {
+    if (IS_SOUND_ON)
+    {
+      SoundOn(3830, 8);
+    }
+  }
+  if (CURRENT_SCREEN == 4 && (millis() - update_time) > 10000)
+  {
+    tft.setTextSize(1);
+    tft.setTextColor(l_text);
+    if (old_d != rtc.getDateStr(FORMAT_LONG, FORMAT_LITTLEENDIAN, '/'))
+    {
+      tft.fillRect(35, 102, 60, 6, title_texts);
+      tft.setCursor(35, 102);
+      tft.print(String(rtc.getDateStr()).substring(0, 2));
+      tft.print(" ");
+      tft.print(rtc.getMonthStr(FORMAT_SHORT));
+      tft.print(" ");
+      tft.print(String(rtc.getDateStr()).substring(6));
+    }
+
+    tft.setTextColor(title_texts);
+    tft.fillRect(90, 35, 109, 22, title_bg);
+    tft.setTextSize(3);
+    tft.setCursor(90, 35);
+    tft.print(String(rtc.getTimeStr()).substring(0, 5));
+
+    tft.fillRect(86, 64, 115, 22, title_bg);
+    tft.setCursor(90, 65);
+    if ((int)LST < 10)
+    {
+      tft.print("0");
+      tft.print((int)LST);
+    }
+    else
+    {
+      tft.print((int)LST);
+    }
+    tft.print(":");
+    if ((LST - (int)LST) * 60 < 10)
+    {
+      tft.print("0");
+      tft.print((LST - (int)LST) * 60, 0);
+    }
+    else
+    {
+      tft.print((LST - (int)LST) * 60, 0);
+    }
+    if ((OBJECT_NAME != "") && (OBJECT_NAME != "CP") && (IS_BT_MODE_ON == false))
+    {
+      if (TRACKING_MOON)
+      {
+        planet_pos(10);
+      }
+
+      tft.setCursor(135, 290);
+      tft.setTextSize(2);
       tft.setTextColor(l_text);
-      if (old_d != rtc.getDateStr(FORMAT_LONG, FORMAT_LITTLEENDIAN, '/')){
-            tft.fillRect(35,102,60,6, title_texts);
-            tft.setCursor(35,102);
-            tft.print(String(rtc.getDateStr()).substring(0,2));
-            tft.print(" ");
-            tft.print(rtc.getMonthStr(FORMAT_SHORT));
-            tft.print(" ");
-            tft.print(String(rtc.getDateStr()).substring(6));
-      }
-      tft.setTextColor(title_texts);
-      tft.fillRect(90,35,109,22, title_bg);
-      tft.setTextSize(3);
-      tft.setCursor(90, 35);
-      tft.print(String(rtc.getTimeStr()).substring(0,5));  
-           
-      tft.fillRect(90,65,109,22, title_bg);
-      tft.setCursor(90, 65);
-      if ((int)LST < 10){
-        tft.print("0");
-        tft.print((int)LST);
-      }else{
-        tft.print((int)LST);
-      }
-      tft.print(":");
-      if ((LST - (int)LST)*60 < 10){
-        tft.print("0");
-        tft.print((LST - (int)LST)*60, 0);
-      }else{
-        tft.print((LST - (int)LST)*60, 0);
-      }
-      if ((OBJECT_NAME != "") && (OBJECT_NAME != "CP") && (IS_BT_MODE_ON == false)){
-          tft.setCursor(135,290);
-          tft.setTextSize(2);
-          tft.setTextColor(l_text);
-          tft.fillRect(133,290,112,20, BLACK);
-          tft.print(HAHour,0);
-          tft.print("h");
-          tft.print(HAMin,1);
-          tft.print("m");
+      tft.fillRect(133, 290, 112, 20, BLACK);
+      tft.print(HAHour, 0);
+      tft.print("h");
+      tft.print(HAMin, 1);
+      tft.print("m");
 
           tft.setTextSize(1);
-          tft.setCursor(2,240);
+          tft.setCursor(2,261);
           tft.setTextColor(l_text);
-          tft.fillRect(1,239,200,18, BLACK);
+          tft.fillRect(1,260,200,10, BLACK);
           tft.print("Az/Alt: ");
           tft.print(floor(AZ), 0);
           tft.print("^ ");
@@ -123,191 +148,191 @@ void considerTimeUpdates(){   // UPDATEs time on Screen1 && Screen4 -  Clock Scr
           tft.print("^ ");
           tft.print((ALT - floor(ALT))*60, 0);
           tft.print("'");
-      }
+    }
 
-      if ((IS_BT_MODE_ON == true) && (OBJECT_DESCR == "Pushed via BlueTooth")){
-          tft.setTextSize(1);
-          tft.setCursor(5,256);
-          tft.setTextColor(l_text);
-          tft.fillRect(5,255,230,22, BLACK);
-          tft.print("HA: ");
-          tft.print(HAHour,0);
-          tft.print("h");
-          tft.print(HAMin,1);
-          tft.print("m    | HS:");
-          tft.println(SLEW_RA_microsteps);
-          tft.print("               | CS:");
-          tft.print(RA_microSteps);
-      }   
-          
-      update_time = millis();
-  }
-  else if (CURRENT_SCREEN == 5 && (millis()-update_time) > 2000)
-  {
-      Current_RA_DEC();
-      tft.setTextSize(3);
-      
-      tft.fillRect(87,77,198,30, btn_d_border);
-      tft.setCursor(108, 85);
-      tft.print(curr_DEC_lz);
-
-      tft.fillRect(87,132,198,30, btn_d_border);
-      tft.setCursor(115, 140);
-      tft.print(curr_HA_lz);
-
-      tft.fillRect(87,187,198,30, btn_d_border);
-      tft.setCursor(115, 195);
-      tft.print(curr_RA_lz);
-
-      update_time = millis();  
-
-   }
-   else if (CURRENT_SCREEN == 1 && (millis()-update_time) > 10000 && changes == 0)
-   {
-      tft.setCursor(110, 115);
+    if ((IS_BT_MODE_ON == true) && (OBJECT_DESCR == "Pushed via BlueTooth")) {
+      tft.setTextSize(1);
+      tft.setCursor(5, 256);
       tft.setTextColor(l_text);
-      tft.fillRect(110,115,200,30, BLACK);
-      tft.print(rtc.getTimeStr(FORMAT_SHORT));
-      if (old_d != rtc.getDateStr(FORMAT_LONG, FORMAT_LITTLEENDIAN, '/')){
-          tft.fillRect(70,80,200,30, BLACK);
-          tft.setCursor(70, 80);
-          tft.print(rtc.getDateStr(FORMAT_LONG, FORMAT_LITTLEENDIAN, '/'));
+      tft.fillRect(5, 255, 230, 22, BLACK);
+      tft.print("HA: ");
+      tft.print(HAHour, 0);
+      tft.print("h");
+      tft.print(HAMin, 1);
+      tft.print("m    | HS:");
+      tft.println(SLEW_RA_microsteps);
+      tft.print("               | CS:");
+      tft.print(RA_microSteps);
+    }
+
+    update_time = millis();
+  }
+  else if (CURRENT_SCREEN == 5 && (millis() - update_time) > 2000)
+  {
+    Current_RA_DEC();
+    tft.setTextSize(3);
+
+    tft.fillRect(87, 77, 198, 30, btn_d_border);
+    tft.setCursor(108, 85);
+    tft.print(curr_DEC_lz);
+
+    tft.fillRect(87, 132, 198, 30, btn_d_border);
+    tft.setCursor(115, 140);
+    tft.print(curr_HA_lz);
+
+    tft.fillRect(87, 187, 198, 30, btn_d_border);
+    tft.setCursor(115, 195);
+    tft.print(curr_RA_lz);
+
+    update_time = millis();
+
+  }
+  else if (CURRENT_SCREEN == 1 && (millis() - update_time) > 10000 && changes == 0)
+  {
+    tft.setCursor(110, 115);
+    tft.setTextColor(l_text);
+    tft.fillRect(110, 115, 200, 30, BLACK);
+    tft.print(rtc.getTimeStr(FORMAT_SHORT));
+    if (old_d != rtc.getDateStr(FORMAT_LONG, FORMAT_LITTLEENDIAN, '/')) {
+      tft.fillRect(70, 80, 200, 30, BLACK);
+      tft.setCursor(70, 80);
+      tft.print(rtc.getDateStr(FORMAT_LONG, FORMAT_LITTLEENDIAN, '/'));
+    }
+    update_time = millis();
+  }
+  else if (CURRENT_SCREEN == 0 && (millis() - update_time) > 5000)
+  {
+    tft.fillRect(10, 200, 320, 200, BLACK);
+    tft.setTextColor(btn_l_text);
+    tft.setTextSize(3);
+    tft.setCursor(55, 200);
+    tft.print("LAT: ");
+    tft.print(gps.location.lat(), 4);
+    tft.setCursor(55, 260);
+    tft.print("LNG: ");
+    tft.print(gps.location.lng(), 4);
+    tft.setCursor(85, 300);
+    tft.setTextSize(2);
+    tft.print("Satellites: ");
+    tft.print(gps.satellites.value());
+    tft.setCursor(110, 325);
+    tft.print("GMT: ");
+    tft.print(gps.time.hour());
+    tft.print(":");
+    tft.print(gps.time.minute());
+    tft.setCursor(75, 350);
+    tft.print("Altitude: ");
+    tft.print(gps.altitude.meters());
+    if (gps.satellites.value() == 0)
+    {
+      smartDelay(1000);
+    }
+    else
+    {
+      GPS_iterrations += 1;
+      smartDelay(1000);
+    }
+
+    if ((GPS_iterrations > 2) && (gps.location.lat() != 0))
+    {
+      OBSERVATION_LONGITUDE = gps.location.lng();
+      OBSERVATION_LATTITUDE = gps.location.lat();
+      OBSERVATION_ALTITUDE = gps.altitude.meters();
+      // Set the earth rotation direction depending on the Hemisphere...
+      // HIGH and LOW are substituted
+      if (OBSERVATION_LATTITUDE > 0)
+      {
+        STP_FWD = LOW;
+        STP_BACK = HIGH;
       }
-      update_time = millis();      
+      else
+      {
+        STP_FWD = HIGH;
+        STP_BACK = LOW;
+      }
+      Serial.println(OBSERVATION_LATTITUDE);
+      Serial.print("STP_FWD = ");
+      Serial.println(STP_FWD);
+      Serial.print("STP_BACK = ");
+      Serial.println(STP_BACK);
+      if (IS_SOUND_ON) {
+        SoundOn(note_C, 32);
+      }
+      CURRENT_SCREEN = 1;
+      // Serial2.end();
+
+      int ora, date_delay = 0;
+      int time_delay = round(gps.location.lng() * 4 / 60); //rough calculation of the timezone delay
+
+      // convert to epoch
+      setTime(gps.time.hour(), gps.time.minute(), gps.time.second(), gps.date.day(), gps.date.month(), gps.date.year());
+#ifdef serial_debug
+      Serial.print("epoch: ");
+      Serial.println(now());
+#endif
+
+      if (isSummerTime())
+      {
+        //If in summer time sum 1h and put Summer_Time flag as 1
+        time_delay += 1;
+        Summer_Time = 1;
+      }
+
+      //update the value of the variable ora
+      ora = gps.time.hour() + time_delay;
+
+      //to update the real time
+      if (ora >= 24)
+      {
+        ora -= 24;
+        date_delay = 1;
+      }
+
+      //setTime(ora, gps.time.minute(), gps.time.second(), gps.date.day()+date_delay, gps.date.month(), gps.date.year()); <-- too mantain sync with UTC
+      rtc.setDate(gps.date.day() + date_delay, gps.date.month(), gps.date.year());
+      rtc.setTime(ora, gps.time.minute(), gps.time.second());
+
+      drawClockScreen();
+    }
+    update_time = millis();
   }
-  else if (CURRENT_SCREEN == 0 && (millis()-update_time) > 5000)
+  else if ((CURRENT_SCREEN == 13) && (IS_OBJ_FOUND == true) && ((millis() - update_time) > 2000))
   {
-        tft.fillRect(10,200,320,200, BLACK);
-        tft.setTextColor(btn_l_text);
-        tft.setTextSize(3);
-        tft.setCursor(55, 200);
-        tft.print("LAT: ");
-        tft.print(gps.location.lat(),4);
-        tft.setCursor(55, 260);
-        tft.print("LNG: ");
-        tft.print(gps.location.lng(),4);
-        tft.setCursor(85, 300);
-        tft.setTextSize(2);
-        tft.print("Satellites: ");
-        tft.print(gps.satellites.value());
-        tft.setCursor(110, 325);
-        tft.print("GMT: ");
-        tft.print(gps.time.hour());
-        tft.print(":");
-        tft.print(gps.time.minute());
-        tft.setCursor(75, 350);
-        tft.print("Altitude: ");
-        tft.print(gps.altitude.meters());
-        if (gps.satellites.value()==0)
-        {
-          smartDelay(1000);
-        }
-        else
-        {
-          GPS_iterrations += 1;
-          smartDelay(1000);
-        }
+    tft.setTextColor(btn_l_text);
+    tft.setTextSize(2);
+    float HAHh;
+    float HAMm;
+    float HA_deci;
 
-         if ((GPS_iterrations > 2) && (gps.location.lat() != 0))
-         {
-          OBSERVATION_LONGITUDE = gps.location.lng();
-          OBSERVATION_LATTITUDE = gps.location.lat();
-          OBSERVATION_ALTITUDE = gps.altitude.meters();
-          // Set the earth rotation direction depending on the Hemisphere...
-          // HIGH and LOW are substituted 
-          if (OBSERVATION_LATTITUDE > 0)
-          {
-            STP_FWD = LOW;
-            STP_BACK = HIGH;
-          }
-          else
-          {
-            STP_FWD = HIGH;
-            STP_BACK = LOW;
-          }
-          Serial.println(OBSERVATION_LATTITUDE);
-          Serial.print("STP_FWD = ");
-          Serial.println(STP_FWD);
-          Serial.print("STP_BACK = ");
-          Serial.println(STP_BACK);
-          if (IS_SOUND_ON){
-            SoundOn(note_C,32);
-          }
-          CURRENT_SCREEN = 1;
-          // Serial2.end();
+    if (HAHour >= 12) {
+      HAHh = HAHour - 12;
+    } else {
+      HAHh = HAHour;
+    }
 
-          int ora, date_delay = 0;
-          int time_delay = round(gps.location.lng() * 4/60); //rough calculation of the timezone delay
-          
-          // convert to epoch
-          setTime(gps.time.hour(), gps.time.minute(), gps.time.second(), gps.date.day(), gps.date.month(), gps.date.year());
-          #ifdef serial_debug
-            Serial.print("epoch: ");
-            Serial.println(now());
-          #endif
-          
-          if(isSummerTime())
-          {
-            //If in summer time sum 1h and put summer_time flag as 1
-            time_delay += 1;
-            Summer_Time = 1;
-          }
-            
-          //update the value of the variable ora
-          ora = gps.time.hour() + time_delay;
+    HAMm = HAMin;
+    HA_deci = (HAHh + (HAMm / 60)) * 15; // In degrees - decimal
 
-          //to update the real time
-          if(ora >= 24)
-          {
-            ora -= 24;
-            date_delay = 1;
-          }
 
-          setTime(ora, gps.time.minute(), gps.time.second(), gps.date.day()+date_delay, gps.date.month(), gps.date.year());
-          rtc.setDate(gps.date.day()+date_delay, gps.date.month(), gps.date.year());
-          rtc.setTime(ora, gps.time.minute(), gps.time.second());
-          
-          drawClockScreen();
-        }
-        update_time = millis(); 
+    if (ALLIGN_STEP == 1)
+    {
+      tft.fillRect(0, 295, 320, 80, BLACK);
+      tft.setCursor(0, 300);
+      tft.setTextSize(2);
+
+      delta_a_RA = (double(RA_microSteps) - double(HA_deci * HA_H_CONST)) / double(HA_H_CONST);
+      delta_a_DEC = (double(DEC_microSteps) - double(SLEW_DEC_microsteps)) / double(DEC_D_CONST);
+
+      tft.print("Delta_RA: ");
+      tft.print(delta_a_RA * 60, 6);
+      tft.println(" arcmin");
+      tft.print("Delta_DEC: ");
+      tft.print(delta_a_DEC * 60, 6);
+      tft.println(" arcmin");
+
+    }
+    tft.setTextSize(2);           // To make sure that when a button is Pressed, it will be re-drawn sith Size 2 text!
+    update_time = millis();
   }
-  else if ((CURRENT_SCREEN == 13) && (IS_OBJ_FOUND == true) && ((millis()-update_time) > 2000))
-  {
-       tft.setTextColor(btn_l_text);
-       tft.setTextSize(2);
-       float HAHh;
-       float HAMm;
-       float HA_deci;
-       
-       if (HAHour >= 12){
-          HAHh = HAHour - 12;
-       }else{
-          HAHh = HAHour;
-       }
 
-       HAMm = HAMin;
-       HA_deci = (HAHh+(HAMm/60))*15;   // In degrees - decimal
-
-
-        if (ALLIGN_STEP == 1)
-        {                  
-            tft.fillRect(0, 295, 320, 80, BLACK);
-            tft.setCursor(0, 300);
-            tft.setTextSize(2);
-
-            delta_a_RA = (double(RA_microSteps) - double(HA_deci * HA_H_CONST))/double(HA_H_CONST);
-            delta_a_DEC = (double(DEC_microSteps) - double(SLEW_DEC_microsteps))/double(DEC_D_CONST);
-          
-            tft.print("Delta_RA: ");
-            tft.print(delta_a_RA*60, 6);
-            tft.println(" arcmin");
-            tft.print("Delta_DEC: ");
-            tft.print(delta_a_DEC*60, 6);
-            tft.println(" arcmin");
-            
-        }
-        tft.setTextSize(2);           // To make sure that when a button is Pressed, it will be re-drawn sith Size 2 text!
-        update_time = millis();
-  }
-    
 }
